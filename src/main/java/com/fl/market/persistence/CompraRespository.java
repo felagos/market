a749d5921097439660f4bd8610b0a63e0d96b252
Felipe Lagos <felagossanchez@gmail.com>
2020-10-24 21:32:46 -0300

    quito print

--- src/main/java/com/fl/market/persistence/CompraRespository.java
+++ src/main/java/com/fl/market/persistence/CompraRespository.java
@@ -34,8 +34,7 @@ public class CompraRespository implements PurchaseRepository {
     @Override
     public Purchase save(Purchase purchase) {
         var compra = mapper.toCompra(purchase);
-        System.out.println("compra " + compra);
-        compra.getProductos().forEach(producto -> producto.setCompra(compra));
+         compra.getProductos().forEach(producto -> producto.setCompra(compra));
         return mapper.toPurchase(compraRespository.save(compra));
     }
 
