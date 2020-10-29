84d2f5007b48f952f508dcef7e9fbec7f6bd3742
Felipe Lagos <felagossanchez@gmail.com>
2020-10-29 01:10:45 -0300

    adjusment variables

--- src/test/java/com/fl/market/integration/controller/CategoryIntegrationTest.java
+++ src/test/java/com/fl/market/integration/controller/CategoryIntegrationTest.java
@@ -2,6 +2,7 @@ package com.fl.market.integration.controller;
 
 import com.fl.market.web.response.ResponseController;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -16,11 +17,18 @@ public class CategoryIntegrationTest {
     private TestRestTemplate restTemplate;
 
     @LocalServerPort
-    private int port;
+    private int PORT;
+
+    private String URL;
+
+    @BeforeEach
+    public void init() {
+        URL = "http://localhost:" + PORT;
+    }
 
     @Test
     public void test() throws Exception {
-        var response = restTemplate.getForEntity("http://localhost:" + port + "/api/category/8", ResponseController.class);
+        var response = restTemplate.getForEntity(URL + "/api/category/8", ResponseController.class);
 
         Assertions.assertEquals(response.getStatusCode(), HttpStatus.OK);
         Assertions.assertNotEquals(response.getBody().getData(), null);
