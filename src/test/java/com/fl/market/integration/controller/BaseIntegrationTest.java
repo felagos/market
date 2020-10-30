4bdcea7ff8fa2da766386336882add7ac27a92a3
Felipe Lagos <felagossanchez@gmail.com>
2020-10-30 10:59:01 -0300

    iintegration test controller

--- src/test/java/com/fl/market/integration/controller/BaseIntegrationTest.java
+++ src/test/java/com/fl/market/integration/controller/BaseIntegrationTest.java
@@ -1,2 +1,21 @@
-package com.fl.market.integration.controller;public class BaseIntegrationTest {
+package com.fl.market.integration.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+
+@SpringBootTest(webEnvironment= SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class BaseIntegrationTest {
+
+    @Autowired
+    protected TestRestTemplate restTemplate;
+
+    @LocalServerPort
+    protected int PORT;
+
+    protected String createURLWithPort(String uri) {
+        return new StringBuilder("http://localhost:").append(PORT).append(uri).toString();
+    }
+
 }
--- src/test/java/com/fl/market/integration/controller/CategoryIntegrationTest.java
+++ src/test/java/com/fl/market/integration/controller/CategoryIntegrationTest.java
@@ -2,33 +2,14 @@ package com.fl.market.integration.controller;
 
 import com.fl.market.web.response.ResponseController;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-public class CategoryIntegrationTest {
-
-    @Autowired
-    private TestRestTemplate restTemplate;
-
-    @LocalServerPort
-    private int PORT;
-
-    private String URL;
-
-    @BeforeEach
-    public void init() {
-        URL = "http://localhost:" + PORT;
-    }
+public class CategoryIntegrationTest extends BaseIntegrationTest {
 
     @Test
     public void test() throws Exception {
-        var response = restTemplate.getForEntity(URL + "/api/category/8", ResponseController.class);
+        var response = restTemplate.getForEntity(createURLWithPort("/api/category/8"), ResponseController.class);
 
         Assertions.assertEquals(response.getStatusCode(), HttpStatus.OK);
         Assertions.assertNotEquals(response.getBody().getData(), null);
